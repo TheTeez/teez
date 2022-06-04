7a8eb0dc893167f1ff5500d60eefdd668629ea55
ShineiIchijo <johncena795001@gmail.com>
2022-06-04 17:24:55 +0530

    chore: add database connection && models

---
+++ src/Database/connection.js
@@ -0,0 +1,33 @@
+const { default: mongoose } = require("mongoose")
+
+module.exports = class connect {
+    /**
+     * @param {string} uri 
+     */
+    constructor(uri) {
+        /**
+         * @type {string}
+         */
+        this.uri = uri
+    }
+
+    /**
+     * @returns {Promise<mongoose.Connection>}
+     */
+
+    connect = async () => {
+        if (!this.URI || this.URI === "")
+      throw new Error('No MongoDB URI provided');
+    const db = mongoose.connection;
+    mongoose.connect(encodeURI(this.URI));
+    return await new Promise((resolve, reject) => {
+      db.on("error", (error) => {
+        reject(error);
+      });
+
+      db.once("open", () => {
+        resolve(db);
+      });
+    });
+  };
+    }
---
+++ src/Database/index.js
@@ -0,0 +1,5 @@
+module.exports = {
+    userSchema: require('./Models/User'),
+    groupSchema: require('./Models/Group'),
+    connection: require('./connection')
+}
